b58fef086a14203ba45066fe126aec0207b5a9e4
Gregory Beurier <gregory.beurier@gmail.com>
2025-05-13 11:13:18 +0200

    Refactor workflows and update dependencies; add nirs4all logo and remove pinard logo

--- nirs4all/__init__.py
+++ nirs4all/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "2.0.0b4"
+__version__ = "0.0.1"
 
-__all__ = ["core", "data", "data_splitters", "presets", "transformations"]
+__all__ = ["core", "data", "data_splitters", "presets", "transformations", "utils"]
