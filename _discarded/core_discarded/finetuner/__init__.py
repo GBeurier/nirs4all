8282b029d25729978a4646df3a9c0f526b98969a
Gregory Beurier <gregory.beurier@gmail.com>
2025-06-07 02:49:32 +0200

    dataset clean

--- _discarded/core_discarded/finetuner/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Finetuner module for optimization of machine learning models.
-"""
-
-from nirs4all.core.finetuner.base_finetuner import *
-
-__all__ = ["BaseFinetuner", "OptunaFineTuner", "SklearnFineTuner", "FineTunerFactory"]
