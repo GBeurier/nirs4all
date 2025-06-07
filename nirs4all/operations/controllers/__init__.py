10223e31e4b0629c9b0b6011a209d5ef42818f31
Gregory Beurier <beurier@cirad.fr>
2025-06-07 03:51:22 +0200

    Folder structure updated

--- nirs4all/operations/controllers/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Controllers module for nirs4all operations.
-
-This module contains controller classes for operations.
-Note: Most files in this module are currently empty and under development.
-"""
-
-__all__ = []
--- nirs4all/operations/monitoring/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Monitoring module for nirs4all operations.
-
-This module contains monitoring classes for operations.
-Note: Most files in this module are currently empty and under development.
-"""
-
-__all__ = []
