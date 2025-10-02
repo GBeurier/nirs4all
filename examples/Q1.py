4d682268c21ab5c40b72bf0c42bf8502b34fb3e2
Gregory Beurier <gregory.beurier@gmail.com>
2025-10-02 17:52:01 +0200

    refactor: Adjust feature augmentation count in Q1.py and add metric comparison for better performance analysis in prediction_analyzer.py

--- examples/Q1.py
+++ examples/Q1.py
@@ -21,11 +21,11 @@ pipeline = [
     "chart_2d",
     x_scaler,
     {"y_processing": y_scaler},
-    {"feature_augmentation": {"_or_": list_of_preprocessors, "size": [1, (1, 2)], "count": 2}},  # Generate all elements of size 1 and of order 1 or 2 (ie. "Gaussian", ["SavitzkyGolay", "Log"], etc.)
+    {"feature_augmentation": {"_or_": list_of_preprocessors, "size": [1, (1, 2)], "count": 10}},  # Generate all elements of size 1 and of order 1 or 2 (ie. "Gaussian", ["SavitzkyGolay", "Log"], etc.)
     splitting_strategy,
 ]
 
-for i in range(5, 35, 10):
+for i in range(5, 35, 2):
     model = {
         "name": f"PLS-{i}_cp",
         "model": PLSRegression(n_components=i)
