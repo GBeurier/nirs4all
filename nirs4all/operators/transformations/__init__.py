4dbe990a3d19094b95a39572e6988741abe99a7d
Gregory Beurier <beurier@cirad.fr>
2025-10-15 14:11:05 +0200

    fix: import statements and return types in presets

--- nirs4all/operators/transformations/__init__.py
+++ nirs4all/operators/transformations/__init__.py
@@ -1,23 +1,67 @@
-from ..augmentation.random import (Random_X_Operation,
-                                   Rotate_Translate)
-from ..augmentation.splines import (Spline_Curve_Simplification,
-                                   Spline_X_Simplification,
-                                   Spline_Y_Perturbations,
-                                   Spline_X_Perturbations,
-                                   Spline_Smoothing)
+from ..augmentation.random import (
+    Random_X_Operation,
+    Rotate_Translate,
+)
+from ..augmentation.splines import (
+    Spline_Curve_Simplification,
+    Spline_X_Simplification,
+    Spline_Y_Perturbations,
+    Spline_X_Perturbations,
+    Spline_Smoothing,
+)
 from ..augmentation.abc_augmenter import Augmenter, IdentityAugmenter
 
-from sklearn.preprocessing import FunctionTransformer as IdentityTransformer
-from sklearn.preprocessing import RobustScaler as RobustNormalVariate
+from .nirs import (
+    Haar,
+    MultiplicativeScatterCorrection,
+    SavitzkyGolay,
+    Wavelet,
+    msc,
+    savgol,
+    wavelet_transform,
+    LogTransform,
+    FirstDerivative,
+    SecondDerivative,
+    log_transform,
+    first_derivative,
+    second_derivative,
+)
 
-from .nirs import (Haar, MultiplicativeScatterCorrection, SavitzkyGolay, Wavelet, msc, savgol,
-                   wavelet_transform, LogTransform, FirstDerivative, SecondDerivative,
-                   log_transform, first_derivative, second_derivative)
-from .scalers import (Derivate, Normalize, SimpleScale, derivate, norml, spl_norml,
-                      StandardNormalVariate)
+# Import scalers (including local aliases such as IdentityTransformer and
+# RobustNormalVariate which are defined in the scalers module)
+from .scalers import (
+    IdentityTransformer,
+    RobustNormalVariate,
+    Derivate,
+    Normalize,
+    SimpleScale,
+    derivate,
+    norml,
+    spl_norml,
+    StandardNormalVariate,
+)
 from .signal import Baseline, Detrend, Gaussian, baseline, detrend, gaussian
 from .features import CropTransformer, ResampleTransformer
 from .resampler import Resampler
+from .presets import (
+    id_preprocessing,
+    savgol_only,
+    haar_only,
+    nicon_set,
+    decon_set,
+    senseen_set,
+    transf_set,
+    special_set,
+    small_set,
+    dumb_set,
+    dumb_and_dumber_set,
+    dumb_set_2D,
+    list_of_2D_sets,
+    optimal_set_2D,
+    preprocessing_list,
+    fat_set,
+)
+from .targets import IntegerKBinsDiscretizer, RangeDiscretizer
 
 
 __all__ = [
@@ -73,5 +117,26 @@ __all__ = [
     "ResampleTransformer",
 
     # Wavelength resampling
-    "Resampler"
+    "Resampler",
+    # Targets / discretizers
+    "IntegerKBinsDiscretizer",
+    "RangeDiscretizer",
+
+    # Preset sets and helpers
+    "id_preprocessing",
+    "savgol_only",
+    "haar_only",
+    "nicon_set",
+    "decon_set",
+    "senseen_set",
+    "transf_set",
+    "special_set",
+    "small_set",
+    "dumb_set",
+    "dumb_and_dumber_set",
+    "dumb_set_2D",
+    "list_of_2D_sets",
+    "optimal_set_2D",
+    "preprocessing_list",
+    "fat_set",
 ]
