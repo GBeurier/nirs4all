63859124bea56bf33307db6de3c0db18c85c7a78
Gregory Beurier <beurier@cirad.fr>
2025-11-25 14:44:27 +0100

    chore: update version to 0.5.1 and document charts performance fix in changelog

--- nirs4all/__init__.py
+++ nirs4all/__init__.py
@@ -4,7 +4,7 @@ NIRS4All - A comprehensive package for Near-Infrared Spectroscopy data processin
 This package provides tools for spectroscopy data handling, preprocessing, model building,
 and pipeline management with support for multiple ML backends.
 """
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 
 import os
 os.environ['DISABLE_EMOJIS'] = '1'
